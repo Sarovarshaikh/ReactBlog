3b099bc2089c1cfd5f30df8e19cd048d4c59222c
Sarovar Shaikh <sarwarshaikh3622@gmail.com>
2022-09-23 22:55:23 +0530

    My First Project Of React Blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import Home from './Components/Home';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Technology from './Components/Technology';
+import Bollywood from './Components/Bollywood';
+import Hollywood from './Components/Hollywood';
+import Fitness from './Components/Fitness';
+import Food from './Components/Food';
+import NavBar from './Components/NavBar';
+import Data from './Components/Data';
+import BlogDetail from './Components/Blog-Detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Data>
+      <BrowserRouter>
+      <NavBar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/category/bollywood" element={<Bollywood />} />
+          <Route path="/category/technology" element={<Technology />} />
+          <Route path="/category/hollywood" element={<Hollywood />} />
+          <Route path="/category/fitness" element={<Fitness />} />
+          <Route path="/category/food" element={<Food />} />
+          <Route path="/bollywood/:id" element={<BlogDetail />} />
+          <Route path="/technology/:id" element={<BlogDetail />} />
+          <Route path="/hollywood/:id" element={<BlogDetail />} />
+          <Route path="/fitness/:id" element={<BlogDetail />} />
+          <Route path="/food/:id" element={<BlogDetail />} />
+
+        </Routes>
+      </BrowserRouter>
+    </Data>
+    
+
+    
+  </div>
   );
 }
 
